ff9d60cc4554fe6ae893df34ca4ae3e1a7c9ffbe
jacekchess <j.wisniewski@vp.pl>
2021-11-27 16:35:10 +0100

    adding graph to django

--- django/EuropeClustering/application/admin.py
+++ django/EuropeClustering/application/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Variable, Algorithm
+from .models import Variable, Algorithm, Data
 
 admin.site.register(Variable)
 admin.site.register(Algorithm)
+admin.site.register(Data)
