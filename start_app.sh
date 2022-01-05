f9157dc978c5d2dd83e07e262f5cb195c0957b4b
amakarewicz <a.makarewicz@student.mini.pw.edu.pl>
2022-01-05 23:59:03 +0100

    changing path to python env

--- start_app.sh
+++ start_app.sh
@@ -5,5 +5,5 @@ path_to_the_local_python_environment=$1
 #pip install -r requirements.txt
 #"C:/Program Files/R/R-4.0.3/bin/RScript.exe" -e "install.packages('clValid', repos='https://cran.rstudio.com/')"
 #"C:/Program Files/R/R-4.0.3/bin/RScript.exe" -e "install.packages('symbolicDA', repos='https://cran.rstudio.com/')"
-$path_to_the_local_python_environment django/EuropeClustering/manage.py runserver 8000 
+$path_to_the_local_python_environment/Scripts/python.exe django/EuropeClustering/manage.py runserver 8000 
 sleep 30
