412587996f577cdf6d3e71ecfdc8ca28d834b68d
amakarewicz <a.makarewicz@student.mini.pw.edu.pl>
2022-01-05 14:18:13 +0100

    app from script fix + doc installation instruction

--- install_requirements.sh
+++ install_requirements.sh
@@ -4,8 +4,9 @@ path_to_the_local_python_environment=$1
 path_to_the_local_R_environment=$2
 
 source "$path_to_the_local_python_environment/Scripts/activate"
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 "$path_to_the_local_R_environment/bin/RScript.exe" -e "install.packages('clValid', repos='https://cran.rstudio.com/')"
 "$path_to_the_local_R_environment/bin/RScript.exe" -e "install.packages('symbolicDA', repos='https://cran.rstudio.com/')"
 
 echo 'All required packeges installed successfully'
+sleep 30
